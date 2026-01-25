0277ff9814ac79172b8b6f41c1022059387be6e4
fluidpanda <qborrd@gmail.com>
2026-01-25 23:15:29 +0300

    Update eslint configs version to 9

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import tseslint from "typescript-eslint";
+
+export default [
+    {
+        ignores: ["node_modules/**", "dist/**"],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                project: "./tsconfig.eslint.json",
+                sourceType: "module",
+            },
+        },
+        plugins: {
+            import: importPlugin,
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+            "@typescript-eslint/no-explicit-any": "warn",
+            "import/order": [
+                "error",
+                {
+                    groups: ["builtin", "external", "internal", ["parent", "sibling", "index"], "type"],
+                    "newlines-between": "never",
+                    alphabetize: {
+                        order: "asc",
+                        caseInsensitive: true,
+                    },
+                },
+            ],
+        },
+    },
+    prettier,
+];
