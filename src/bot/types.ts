2a7db96078675145a834f5b5094a42945f5de3d0
fluidpanda <qborrd@gmail.com>
2026-02-01 22:59:51 +0300

    Add new type for bot state and bind it to BotCtx

--- src/bot/types.ts
+++ src/bot/types.ts
@@ -1,4 +1,5 @@
 import type { MenuId } from "@/bot/menu/register";
+import type { AppConfig } from "@/config";
 import type { Logger } from "@/logging/logger";
 import type { Context } from "telegraf";
 import type { Update } from "telegraf/types";
@@ -10,6 +11,7 @@ export interface BotSession {
 export type BotCtx = Context<Update> & {
     session: BotSession;
     state: {
+        config: AppConfig;
         logger?: Logger;
         replyWithMenu?: (text: string) => Promise<void>;
         updateId?: string;
